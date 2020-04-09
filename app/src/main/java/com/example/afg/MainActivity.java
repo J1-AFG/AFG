79a068d88d6bbc27923bb8edcec232c82e5237a5
carolinequinn <cequinn@wpi.edu>
2020-04-09 10:10:17 -0400

    UI part 1 and 2 with dropdown v1

--- app/src/main/java/com/example/afg/MainActivity.java
+++ app/src/main/java/com/example/afg/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,4 +20,15 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, times.class);
         startActivity(intent);
     }
+
+    Spinner spinner1= (Spinner) findViewById(R.id.morningDropdown);
+    ArrayAdapter <CharSequence> morningAdapter= ArrayAdapter.createFromResource(this, R.array.morningTimes, android.R.layout.simple_spinner_item);
+    morningAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    spinner1.setAdapter(morningAdapter);
+
+    Spinner spinner2= (Spinner) findViewById(R.id.eveningDropdown);
+    ArrayAdapter <CharSequence> eveningAdapter= ArrayAdapter.createFromResource(this, R.array.eveningTimes, android.R.layout.simple_spinner_item);
+    eveningAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    spinner2.setAdapter(eveningAdapter);
+
 }
