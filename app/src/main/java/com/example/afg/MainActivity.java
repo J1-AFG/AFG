579873a5ddc3b580bf8393a5bd0d290d2351057a
carolinequinn <cequinn@wpi.edu>
2020-04-13 09:13:24 -0400

    UI part 1 and 2 with dropdown v1

--- app/src/main/java/com/example/afg/MainActivity.java
+++ app/src/main/java/com/example/afg/MainActivity.java
@@ -15,23 +15,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinner1= (Spinner) findViewById(R.id.morningDropdown);
-        ArrayAdapter <CharSequence> morningAdapter= ArrayAdapter.createFromResource(this, R.array.morningTimes, android.R.layout.simple_spinner_item);
+        Spinner spinner1 = (Spinner) findViewById(R.id.morningDropdown);
+        ArrayAdapter<CharSequence> morningAdapter = ArrayAdapter.createFromResource(this, R.array.morningTimes, android.R.layout.simple_spinner_item);
         morningAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(morningAdapter);
 
-        Spinner spinner2= (Spinner) findViewById(R.id.eveningDropdown);
-        ArrayAdapter <CharSequence> eveningAdapter= ArrayAdapter.createFromResource(this, R.array.eveningTimes, android.R.layout.simple_spinner_item);
+        Spinner spinner2 = (Spinner) findViewById(R.id.eveningDropdown);
+        ArrayAdapter<CharSequence> eveningAdapter = ArrayAdapter.createFromResource(this, R.array.eveningTimes, android.R.layout.simple_spinner_item);
         eveningAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(eveningAdapter);
     }
-
-    public void goToTimes (View view){
-        Intent intent = new Intent(this, times.class);
-        startActivity(intent);
-    }
-
-
-
-
 }
+
+    //public void goToTimes (View view){
+      //  Intent intent = new Intent(this, times.class);
+        //startActivity(intent);}
+
+
+
+
+
+
