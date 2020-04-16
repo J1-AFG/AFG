2e55b9a9863234446838e9dc92a65842e353c111
carolinequinn <cequinn@wpi.edu>
2020-04-16 13:17:49 -0400

    finished UI v1

--- app/src/main/java/com/example/afg/MainActivity.java
+++ app/src/main/java/com/example/afg/MainActivity.java
@@ -15,15 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinner1 = (Spinner) findViewById(R.id.morningDropdown);
-        ArrayAdapter<CharSequence> morningAdapter = ArrayAdapter.createFromResource(this, R.array.morningTimes, android.R.layout.simple_spinner_item);
-        morningAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner1.setAdapter(morningAdapter);
 
-        Spinner spinner2 = (Spinner) findViewById(R.id.eveningDropdown);
-        ArrayAdapter<CharSequence> eveningAdapter = ArrayAdapter.createFromResource(this, R.array.eveningTimes, android.R.layout.simple_spinner_item);
-        eveningAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner2.setAdapter(eveningAdapter);
     }
 }
 
